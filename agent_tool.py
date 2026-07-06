5449645775ba77e03dfcf62794ceda26deee6e9a
developer <developer@example.com>
2026-07-06 16:04:17 +0800

    new agetn study https://www.bilibili.com/video/BV1UvDYBNE8d?spm_id_from=333.788.player.switch&vd_source=da3b647c5f30d89af994d616d786b25f&p=5

--- agent_tool.py
+++ agent_tool.py
@@ -1,22 +1,24 @@
 from  langchain_core.prompts import ChatPromptTemplate
 from langchain_core.tools import tool
-import os
 from langchain.chat_models import init_chat_model
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.tools import tool
 from langchain_tavily import TavilySearch
 from dotenv import load_dotenv
-from langchain.agents import create_agent
 import requests
-
-load_dotenv()
+from get_env_utils import deepseek_api_key, deepseek_api_url, city_key, weather_api_key, travily_api_key
 # 2. 初始化模型
 model = init_chat_model(
     model="deepseek-v4-flash",
     model_provider="deepseek",
-    api_key=os.getenv("DEEPSEEK_API_KEY"),
+    api_key=deepseek_api_key,
 )
 
+prompts = ChatPromptTemplate.from_messages([
+    ("system", "你是一个天气助手，用户会询问天气相关的问题，你需要调用 weather_search 函数来获取天气信息即可。"),
+    ("human", "{input}")
+])
+    
 
 # 1. 定义工具 (直接使用 @tool 装饰器更简洁)
 @tool
@@ -39,13 +41,13 @@ def weather_search(city:str):
         return f"查询{city}的天气时出错：{e}"
 
 # 3. 绑定工具并创建 Agent
-tools = [weather_search]
-agents = create_agent(model, tools)
-# 调用 Agent
-response = agents.invoke({
-    "messages": [HumanMessage(content="帮我查询今天北京的天气。")]
-})
-
-# 打印最终回答
-print(response["messages"][-1].content)
+llm_with_tools = model.bind_tools([weather_search])         
+        # 使用 initialize_agent 替代不存在的 create_tool_calling_agent
+agent = initialize_agent(
+    tools=[weather_search],
+    llm=llm_with_tools,
+    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    verbose=False,
+    system_message=prompts,
+)
 
